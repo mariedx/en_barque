8fbd835646dd1d2d29c64ae20ec17580bf82cb0a
MarieDechaux <marie.dechaux@gmail.com>
2021-02-08 15:35:53 +0100

    fix mailer sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_LOGIN'],
     :password => ENV['SENDGRID_PWD'],
-    :domain => 'enbarque-orientation.fr',
+    :domain => 'gmail.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
